6a0eef6ce6643d9a570bd05aa093ce3b5fc9e249
KAIQUE LIMA TORRES <102607065+Kaique-Lima-Torres@users.noreply.github.com>
2022-06-08 02:06:54 -0300

    exercicios python

--- pycharm/084.py
+++ pycharm/084.py
@@ -1,29 +1,19 @@
 from time import sleep
-pessoas = list()
-dados = list()
-pessoas_pesso = list()
+galera = list()
+dado = list()
 while True:
-    pessoas.append(str(input('nome: ')))
-    peso = (float(input('peso: ')))
-    continuacao = str(input('deseja continuar? [S/N]')).strip().upper()
-    pessoas.append(peso)
-    pessoas_pesso.append(peso)
-    dados.append(pessoas[:])
-    pessoas.clear()
-    while continuacao not in 'SsNn':
-        continuacao = str(input('Digite uma opcao valida [S/N]')).strip().upper()
-    if continuacao == 'N':
+    dado.append(str(input('nome:')))
+    dado.append(float(input('peso:')))
+    galera.append(dado[:])
+    dado.clear()
+    reposta = str(input('quer continuar? [S/N]')).upper().strip()[0]
+    if reposta in 'N':
         print('programa encerrando!')
         print('aguarde para ver o resultado ')
         sleep(1)
         break
-print(f'ao todo vc cadastrou {len(dados)} pessoas')
-maior_peso = list()
-menor_peso = list()
-for p in dados:
-    if p[1] >= max(pessoas_pesso):
-        maior_peso.append(p[0])
-    elif p[1] <= min(pessoas_pesso):
-        menor_peso.append(p[0])
-print(f'o maior peso foi de {max(pessoas_pesso)}, e as pessaos foram {maior_peso}')
-print(f'o menor peso foi de {min(pessoas_pesso)}, e as pessoas {menor_peso}')
+print(f'ao todo vc cadastrou  pessoas {len(galera)}')
+idx, max_value = max(galera, key=lambda item: item[1])
+print('o peso maximo foi de', max_value, "e foi de ", idx)
+idx, min_value = min(galera, key=lambda item: item[1])
+print('o peso minimo foi de', min_value, "e foi de ", idx)
